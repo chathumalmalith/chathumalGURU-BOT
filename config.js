533ae8c55a6b429ebad71fd4a43c7684982c8bc7
chathumalmalith <141410119+chathumalmalith@users.noreply.github.com>
2023-08-04 20:23:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94765294704', 'me', true],
+  ['94765294704', 'chathumal', true], 
   [''] 
 ] //Number of owners
 
